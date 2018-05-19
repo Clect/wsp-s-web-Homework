cb58a9d6877a398635e969fcedf132c3cf12f522
Edward Li <clect@foxmail.com>
2018-05-19 11:14:42 +0800

    fix as he said

--- js/a.js
+++ js/a.js
@@ -2,22 +2,24 @@ var data = [
     {
         name: 'Simon Smith',
         img: '../img/Artist/1.jpg',
-        Description: '',
+        Description: 'Hello, everyone!<br />I am the computer designer of Storm<br />Hope you guys like my design!',
         product: [
             '../img/best/1.jpg',
             '../img/best/2.jpg',
             '../img/best/3.jpg'
-        ]
+        ],
+        productNum : [1,2,3]
         
     }, {
-        name: 'Royce Alle',
+        name: 'Royce Allen',
         img: '../img/Artist/2.jpg',        
-        Description: '',
+        Description: 'Hello, everyone!<br />I am the phone case designer of Storm.<br />Hope you guys like my design!',
         product: [
             '../img/best/4.jpg',
             '../img/best/5.jpg',
             '../img/best/6.jpg'
-        ]
+        ],
+        productNum : [4,5,6]
     }
 ];
 
@@ -28,12 +30,15 @@ $(function() {
     var img = aInfo.img;
     var Description = aInfo.Description;
     var product = aInfo.product;
+    var productNum = aInfo.productNum;
     var pString = '';
     for (var i = 0; i < product.length; i++) {
         var that = product[i];
-        pString += '<img class="p' + i + '" src="' + that + '" />';
+        var num = productNum[i];
+        pString += '<a href="./productDetail.html?product=' + (num) +'"><img class="p' + i + '" src="' + that + '" />';
     }
     $('h1.name').text(name);
     $('.artistInfo').append('<img src="' + img + '" />');
     $('.product').append(pString);    
+    $('.artistInfo p').append(Description);
 });
